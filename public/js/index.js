6f71747e9ca3df53c0bd3299893d655486d1c97f
Guillermo Farfan <memo56_fb@hotmail.com>
2021-05-26 16:29:44 -0500

    funciona la suma y resta pero no de forma seguida, tambien hay punto

--- public/js/index.js
+++ public/js/index.js
@@ -1,75 +1,134 @@
-// const num0 = document.querySelector("#zero");
-// const num1 = document.querySelector("#one");
-// const num2 = document.querySelector("#two");
-// const num3 = document.querySelector("#three");
-// const num4 = document.querySelector("#four");
-// const num5 = document.querySelector("#five");
-// const num6 = document.querySelector("#six");
-// const num7 = document.querySelector("#seven");
-// const num8 = document.querySelector("#eight");
-// const num9 = document.querySelector("#nine");
-const del = document.querySelector("#delB");
-const reset = document.querySelector("#resetB");
-const equal = document.querySelector("#equalB");
 const screen = document.querySelector(".screen div");
 const numpad = document.querySelector(".numpad");
 let suma = 0;
-const sumar = ()=>{
-    suma += +screen.outerText;
+let resta = 0;
+let multiplicacion = 0;
+let division = 0;
+let igual = 0;
+const resetValues = ()=>{
+    suma = 0;
+    resta = 0;
+    division = 0;
+    multiplicacion = 0;
+    igual = 0;
+}
+const borrarPantalla = ()=>{
     while (screen.firstChild) {
         screen.removeChild(screen.firstChild);
       }
 }
+const borrarPantallaNumeros = ()=>{
+    if(igual || screen.outerText === "0"){
+        borrarPantalla();
+        resetValues();
+    }
+}
+const sumar = () => {
+    if(igual){
+        borrarPantalla()
+        suma = igual;
+        igual = 0;
+    };
+    suma += +screen.outerText;
+    borrarPantalla();
+    return suma;
+};
+const restar = () => {
+    if(igual){
+        borrarPantalla()
+        resta = igual;
+        igual = 0;
+    };
+    if(resta <= 0){
+        resta += +screen.outerText;
+    }else{
+        resta -= +screen.outerText;
+    }
+    borrarPantalla();
+    return resta;
+};
 
-numpad.addEventListener('click',e =>{
-    if(e.target.textContent === "1"){
-        screen.append(1)
-    }
-    if(e.target.textContent === "2"){
-        screen.append(2)
-    }
-    if(e.target.textContent === "3"){
-        screen.append(3)
-    }
-    if(e.target.textContent === "4"){
-        screen.append(4)
-    }
-    if(e.target.textContent === "5"){
-        screen.append(5)
-    }
-    if(e.target.textContent === "6"){
-        screen.append(6)
-    }
-    if(e.target.textContent === "7"){
-        screen.append(7)
-    }
-    if(e.target.textContent === "8"){
-        screen.append(8)
-    }
-    if(e.target.textContent === "9"){
-        screen.append(9)
-    }
-    if(e.target.textContent === "0"){
-        screen.append(0)
-    }
-    if(e.target.textContent === "+"){
-        sumar();
-    }
-    if(e.target.textContent === "="){
-        sumar();
-        // screen.append(suma)
-        // HAY QUE PASAR EL RESULTADO EN GENERAL DE UNA OPERACION, NO EL VALOR 
-    }
-    if(e.target.textContent === "DEL"){
-        if(screen.firstChild){
-        screen.removeChild(screen.lastChild)
-        }
-    }
-    if(e.target.textContent === "RESET"){
-        while (screen.firstChild) {
-            screen.removeChild(screen.firstChild);
-          }
-          suma = 0;
-    }
-})
 
+const equalCases = () => {
+  if (suma) {
+    screen.append(sumar());
+    igual = suma;
+    suma = 0;
+  }
+  if (resta) {
+    screen.append(restar());
+    igual = resta;
+    resta = 0;
+  }
+};
+
+numpad.addEventListener("click", (e) => {
+  if (e.target.textContent === "1") {
+    borrarPantallaNumeros();
+    screen.append(1);
+  }
+  if (e.target.textContent === "2") {
+    borrarPantallaNumeros();
+    screen.append(2);
+  }
+  if (e.target.textContent === "3") {
+    borrarPantallaNumeros();
+    screen.append(3);
+  }
+  if (e.target.textContent === "4") {
+    borrarPantallaNumeros();
+    screen.append(4);
+  }
+  if (e.target.textContent === "5") {
+    borrarPantallaNumeros();
+    screen.append(5);
+  }
+  if (e.target.textContent === "6") {
+    borrarPantallaNumeros();
+    screen.append(6);
+  }
+  if (e.target.textContent === "7") {
+    borrarPantallaNumeros();
+    screen.append(7);
+  }
+  if (e.target.textContent === "8") {
+    borrarPantallaNumeros();
+    screen.append(8);
+  }
+  if (e.target.textContent === "9") {
+    borrarPantallaNumeros();
+    screen.append(9);
+  }
+  if (e.target.textContent === "0") {
+    borrarPantallaNumeros();
+    screen.append(0);
+  }
+  if (e.target.textContent === "+") {
+    sumar();
+  }
+  if (e.target.textContent === "-") {
+    restar();
+  }
+  if (e.target.textContent === "/") {
+    dividir();
+  }
+  if (e.target.textContent === "x") {
+    multiplicar();
+  }
+  if (e.target.textContent === ".") {
+    screen.append(".");
+  }
+
+  if (e.target.textContent === "=") {
+    equalCases();
+  }
+  if (e.target.textContent === "DEL") {
+    if (screen.firstChild) {
+      screen.removeChild(screen.lastChild);
+    }
+  }
+  if (e.target.textContent === "RESET") {
+    borrarPantalla();
+    resetValues();
+  }
+});
